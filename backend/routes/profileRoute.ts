9a9e2484b3365c8fde0680b46da589c18f3f9a8a
Bibek74 <bbekshah789@gmail.com>
2026-02-20 01:25:13 +0545

    Update profile routes

--- backend/routes/profileRoute.ts
+++ backend/routes/profileRoute.ts
@@ -9,6 +9,7 @@ const profileController = new ProfileController();
 profileRouter.get("/me", jwtAuthMiddleware, profileController.getMyProfile);
 profileRouter.put("/update", jwtAuthMiddleware, profileController.updateMyProfile);
 profileRouter.delete("/delete", jwtAuthMiddleware, profileController.deleteMyProfile);
+profileRouter.get("/visit/:id", profileController.getPublicProfileById);
 profileRouter.get("/visit", jwtAuthMiddleware, profileController.visitOthersProfile);
 profileRouter.put("/upload-image", jwtAuthMiddleware, uploadProfilePhoto.single("profileImage"), profileController.uploadProfileImage);
 
