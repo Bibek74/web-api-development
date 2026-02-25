1681a2a78f40ec11cedec69aca864695f77e6086
Bibek74 <bbekshah789@gmail.com>
2026-02-25 14:23:09 +0545

    Backend: Update post routes

--- backend/routes/postRoute.ts
+++ backend/routes/postRoute.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import { jwtAuthMiddleware } from "../utils/jwt.js";
 import { PostController } from "../controllers/postController.js";
+import { uploadPostImage } from "../middlewares/uploadPostImage.js";
 
 const postRouter = express.Router();
 const postController = new PostController();
 
-postRouter.post("/new", jwtAuthMiddleware, postController.addNewPost);
+postRouter.post("/new", jwtAuthMiddleware, uploadPostImage.single("postImage"), postController.addNewPost);
 postRouter.get("/all", postController.getAllPost); // Public endpoint - no auth required
 postRouter.post("/like-unlike/:id", jwtAuthMiddleware, postController.likeUnlikeById);
 postRouter.get("/user/likes/:id", jwtAuthMiddleware, postController.getTotalLikesById);
-postRouter.put("/update/:id", jwtAuthMiddleware, postController.updatePostById);
+postRouter.put("/update/:id", jwtAuthMiddleware, uploadPostImage.single("postImage"), postController.updatePostById);
 postRouter.get("/my-posts", jwtAuthMiddleware, postController.viewMyPost);
 postRouter.delete("/delete-post/:id", jwtAuthMiddleware, postController.deletePostById);
 
